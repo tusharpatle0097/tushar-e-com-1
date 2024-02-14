129a88982edb0062cb9e8f9e2155bc363cf7a46c
Tushar Patle <tusharpatle0097@gmail.com>
2024-02-14 16:36:57 +0530

    ff

--- src/components/home/Header/HeaderBottom.js
+++ src/components/home/Header/HeaderBottom.js
@@ -168,7 +168,7 @@ const HeaderBottom = () => {
             <Link to="/cart">
               <div className="relative">
                 <FaShoppingCart />
-                <span className="absolute font-titleFont top-3 -right-2 text-xs w-4 h-4 flex items-center justify-center rounded-full bg-primeColor text-white">
+                <span className="absolute font-titleFont top-3 -right-2 text-xs w-4 h-4 flex items-center justify-center rounded-full bg-primeColor ">
                   {products.length > 0 ? products.length : 0}
                 </span>
               </div>
